a5a7546f2319c81a2773150d007baf8ffadf1471
Baldius <55040670+Baldius@users.noreply.github.com>
2025-12-31 13:52:06 +0100

    add2

---
+++ data.js
@@ -0,0 +1,760 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01_flur",
+      "name": "01_Flur",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -0.4556390525117866,
+        "pitch": -0.08292849153434823,
+        "fov": 1.0035515625158256
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6564992394835905,
+          "pitch": 0.6650506397069247,
+          "rotation": 0,
+          "target": "1-02_flur"
+        },
+        {
+          "yaw": -2.1822558690985616,
+          "pitch": 0.4738579609402329,
+          "rotation": 0,
+          "target": "2-03_schlafzimmer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3297032060340097,
+          "pitch": -0.46488436916970954,
+          "title": "Gounodstraße 91, 1. OG",
+          "text": "Denkmalgeschützter Altbau (1927)<div>3 m hohe Decken</div><div>Wohnfläche 51 m²</div><div>Fernwärme</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-02_flur",
+      "name": "02_Flur",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -2.080271817845974,
+        "pitch": 0.022041557856923077,
+        "fov": 1.0035515625158256
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0578712321903394,
+          "pitch": 0.6991754144947642,
+          "rotation": 0,
+          "target": "0-01_flur"
+        },
+        {
+          "yaw": 2.6363448413356165,
+          "pitch": 0.5199690274462103,
+          "rotation": 7.0685834705770345,
+          "target": "2-03_schlafzimmer"
+        },
+        {
+          "yaw": -2.748166311524024,
+          "pitch": 0.3275621851942123,
+          "rotation": 0,
+          "target": "4-05_bad"
+        },
+        {
+          "yaw": -1.6579396338946495,
+          "pitch": 0.5085183350592217,
+          "rotation": 0,
+          "target": "7-08_kche"
+        },
+        {
+          "yaw": 0.09028875354888477,
+          "pitch": 0.8693951599207761,
+          "rotation": 0,
+          "target": "10-11_wohnzimmer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03_schlafzimmer",
+      "name": "03_Schlafzimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 3.0680327947666974,
+        "pitch": -0.20498857387844005,
+        "fov": 1.0035515625158256
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06322844014987794,
+          "pitch": 0.5313805515780654,
+          "rotation": 0,
+          "target": "0-01_flur"
+        },
+        {
+          "yaw": 3.013730840063431,
+          "pitch": -0.14405554578554103,
+          "rotation": 0,
+          "target": "3-04_schlafzimmer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.4907347540479794,
+          "pitch": 0.28629373248838874,
+          "title": "Schlafzimmer",
+          "text": "Infinitybett<div>160 x 200 cm</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-04_schlafzimmer",
+      "name": "04_Schlafzimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 0.05560340979805112,
+        "pitch": 0.28733412823238247,
+        "fov": 1.0035515625158256
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14176644994606669,
+          "pitch": 0.6608598759114113,
+          "rotation": 3.141592653589793,
+          "target": "2-03_schlafzimmer"
+        },
+        {
+          "yaw": -0.09078596668431338,
+          "pitch": 0.47104506519494294,
+          "rotation": 0,
+          "target": "0-01_flur"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05_bad",
+      "name": "05_Bad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -0.4689080480317731,
+        "pitch": -0.48344868368232063,
+        "fov": 1.0035515625158256
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.16684632315551085,
+          "pitch": 0.6832357819734156,
+          "rotation": 0,
+          "target": "5-06_bad"
+        },
+        {
+          "yaw": -2.9684216306841336,
+          "pitch": 0.33903480645065187,
+          "rotation": 0,
+          "target": "1-02_flur"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.2921278755095624,
+          "pitch": -0.5832336405794329,
+          "title": "Bad",
+          "text": "Saniert 2025<div>bodengleiche Dusche</div><div>Waschtrockner</div>"
+        }
+      ]
+    },
+    {
+      "id": "5-06_bad",
+      "name": "06_Bad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 0.9469090162131906,
+        "pitch": -0.5869460776791708,
+        "fov": 1.0035515625158256
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1559387703953945,
+          "pitch": 0.8260803544280542,
+          "rotation": 0,
+          "target": "4-05_bad"
+        },
+        {
+          "yaw": -1.7708165302613175,
+          "pitch": 0.30290010123922784,
+          "rotation": 0,
+          "target": "1-02_flur"
+        },
+        {
+          "yaw": 2.293471081525613,
+          "pitch": 1.0655853113969425,
+          "rotation": 5.497787143782138,
+          "target": "6-07_bad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-07_bad",
+      "name": "07_Bad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -0.03054213287549068,
+        "pitch": -1.02011789390275,
+        "fov": 1.0538905185677918
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.116674110563178,
+          "pitch": 0.41798367383044877,
+          "rotation": 0,
+          "target": "4-05_bad"
+        },
+        {
+          "yaw": -3.102741159475851,
+          "pitch": 0.18922112743949349,
+          "rotation": 0,
+          "target": "1-02_flur"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-08_kche",
+      "name": "08_Küche",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -0.9529712215607091,
+        "pitch": -0.018292578756213373,
+        "fov": 1.0538905185677918
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2573744764851753,
+          "pitch": 0.43355634792295916,
+          "rotation": 0,
+          "target": "1-02_flur"
+        },
+        {
+          "yaw": -0.3808052279915195,
+          "pitch": 0.3058996418665725,
+          "rotation": 0,
+          "target": "8-09_kche"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.687419000343672,
+          "pitch": -0.21132030479373043,
+          "title": "Küche",
+          "text": "Einbauküche<div>Backofen bis 300°</div><div>Spülmaschine (Breite 45 cm)</div><div>Geräumiger Kühl-/Gefrierschrank</div><div>Kaffeemaschine</div>"
+        }
+      ]
+    },
+    {
+      "id": "8-09_kche",
+      "name": "09_Küche",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -1.5471050890180358,
+        "pitch": 0.26683945435194545,
+        "fov": 1.0538905185677918
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2928644922941492,
+          "pitch": 0.5526507028782905,
+          "rotation": 5.497787143782138,
+          "target": "7-08_kche"
+        },
+        {
+          "yaw": -2.074698757814554,
+          "pitch": 0.7417209633931297,
+          "rotation": 0.7853981633974483,
+          "target": "9-10_kche"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-10_kche",
+      "name": "10_Küche",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 2.3761825068947857,
+        "pitch": -0.23186997840932122,
+        "fov": 1.0538905185677918
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1976869046765604,
+          "pitch": 0.3550678304075845,
+          "rotation": 11.780972450961727,
+          "target": "8-09_kche"
+        },
+        {
+          "yaw": 2.793343827053234,
+          "pitch": 0.15786699296997853,
+          "rotation": 0,
+          "target": "7-08_kche"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-11_wohnzimmer",
+      "name": "11_Wohnzimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -0.0038613479026405884,
+        "pitch": -0.03354129129611749,
+        "fov": 1.0538905185677918
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.880490521461933,
+          "pitch": 0.26712750246387706,
+          "rotation": 0,
+          "target": "1-02_flur"
+        },
+        {
+          "yaw": -0.6633179799981015,
+          "pitch": 0.2943893123196144,
+          "rotation": 5.497787143782138,
+          "target": "12-13_wohnzimmer"
+        },
+        {
+          "yaw": -0.07482150729758708,
+          "pitch": 0.08283229700730743,
+          "rotation": 0,
+          "target": "13-14_wohnzimmer"
+        },
+        {
+          "yaw": 0.60809119520815,
+          "pitch": 0.27845107326309204,
+          "rotation": 0.7853981633974483,
+          "target": "11-12_wohnzimmer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0501438753377776,
+          "pitch": -0.3505940363487774,
+          "title": "Wohnzimmer",
+          "text": "Drucker<div>Musikanlage</div><div>Beamer</div>"
+        }
+      ]
+    },
+    {
+      "id": "11-12_wohnzimmer",
+      "name": "12_Wohnzimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 0.7946653983637866,
+        "pitch": 0.14226133894561954,
+        "fov": 1.0538905185677918
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04119751754664769,
+          "pitch": 0.3549103676185865,
+          "rotation": 0,
+          "target": "10-11_wohnzimmer"
+        },
+        {
+          "yaw": 0.6211790486237625,
+          "pitch": 0.25910556294265064,
+          "rotation": 0,
+          "target": "12-13_wohnzimmer"
+        },
+        {
+          "yaw": 1.3178104781127988,
+          "pitch": 0.5671642123467358,
+          "rotation": 0.7853981633974483,
+          "target": "13-14_wohnzimmer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-13_wohnzimmer",
+      "name": "13_Wohnzimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 2.679003174853241,
+        "pitch": 0.020485651236842983,
+        "fov": 1.0538905185677918
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0016037948330556,
+          "pitch": 0.6990226097281358,
+          "rotation": 0.7853981633974483,
+          "target": "10-11_wohnzimmer"
+        },
+        {
+          "yaw": -2.4060967922316934,
+          "pitch": 0.36796942146569656,
+          "rotation": 12.566370614359176,
+          "target": "11-12_wohnzimmer"
+        },
+        {
+          "yaw": -3.13366106645241,
+          "pitch": 0.4697160729783274,
+          "rotation": 0,
+          "target": "13-14_wohnzimmer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-14_wohnzimmer",
+      "name": "14_Wohnzimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -3.056430080465649,
+        "pitch": -0.14394193824183432,
+        "fov": 1.0538905185677918
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.058834242811418,
+          "pitch": 0.32638094434579656,
+          "rotation": 0,
+          "target": "14-15_loggia"
+        },
+        {
+          "yaw": -0.768454161396722,
+          "pitch": 0.35967473340869205,
+          "rotation": 0,
+          "target": "10-11_wohnzimmer"
+        },
+        {
+          "yaw": -0.2799609122726672,
+          "pitch": 0.3913023345181408,
+          "rotation": 0.7853981633974483,
+          "target": "12-13_wohnzimmer"
+        },
+        {
+          "yaw": -1.8194723472980066,
+          "pitch": 0.3526375897749592,
+          "rotation": 5.497787143782138,
+          "target": "11-12_wohnzimmer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-15_loggia",
+      "name": "15_Loggia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 1.8499095876190328,
+        "pitch": 0.35193814147183744,
+        "fov": 1.0538905185677918
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7326303795772375,
+          "pitch": 0.6623796719299229,
+          "rotation": 0,
+          "target": "13-14_wohnzimmer"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Gounodstr. 91 | Dez. 2025",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
